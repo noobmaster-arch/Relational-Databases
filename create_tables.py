501a8c65d817be77a408d5a4db73cfafc675a8f9
Savyasach Reddy <savyasach@cse.iitb.ac.in>
2020-10-24 18:59:15 +0530

    TASKS 1,2

--- create_tables.py
+++ create_tables.py
@@ -25,6 +25,56 @@ cur.execute('''CREATE TABLE IF NOT EXISTS MATCH(
     toss_name TEXT, 
     win_type TEXT, 
     man_of_match INT, 
-    win_margin INT
+    win_margin INT,
+    FOREIGN KEY (team1) REFERENCES TEAM(team_id),
+    FOREIGN KEY (team2) REFERENCES TEAM(team_id),
+    FOREIGN KEY (battedfirst) REFERENCES TEAM(team_id),
+    FOREIGN KEY (battedsecond) REFERENCES TEAM(team_id)
 );''')
 
+
+cur.execute('''CREATE TABLE IF NOT EXISTS PLAYER(
+    player_id INT PRIMARY KEY,
+    player_name TEXT,
+    dob TIMESTAMP,
+    batting_hand TEXT,
+    bowling_skill TEXT,
+    country_name TEXT
+)''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS PLAYER_MATCH(
+    playermatch_key INT PRIMARY KEY,
+    match_id INT ,
+    player_id INT ,
+    batting_hand TEXT,
+    bowling_skill TEXT,
+    role_desc TEXT,
+    team_id INT,
+    FOREIGN KEY (match_id) REFERENCES MATCH(match_id),
+    FOREIGN KEY (player_id) REFERENCES PLAYER(player_id),
+    FOREIGN KEY (team_id) REFERENCES TEAM(team_id)
+)''')
+
+
+cur.execute('''CREATE TABLE IF NOT EXISTS BALL_BY_BALL(
+    match_id INT,
+    innings_no INT,
+    over_id INT,
+    ball_id INT,
+    striker_batting_position INT,
+    runs_scored INT,
+    extra_runs INT,
+    out_type TEXT,
+    striker INT,
+    non_striker INT,
+    bowler INT,
+    PRIMARY KEY (match_id, innings_no, over_id, ball_id),
+    FOREIGN KEY (match_id) REFERENCES MATCH(match_id),
+    FOREIGN KEY (striker) REFERENCES PLAYER(player_id),
+    FOREIGN KEY (non_striker) REFERENCES PLAYER(player_id),
+    FOREIGN KEY (bowler) REFERENCES PLAYER(player_id)
+)''')
+
+# cur.execute('select team1, city_name from MATCH')
+# for row in cur: #just testing
+#     print(row)
--- insert.py
+++ insert.py
@@ -16,6 +16,24 @@ for row in csvReader:
     cur.execute('insert into MATCH(match_id, season_year, team1, team2, battedfirst, battedsecond, venue_name, city_name, country_name, toss_winner, match_winner, toss_name, win_type, man_of_match, win_margin) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', row)
 
 
-cur.execute('select team1, city_name from MATCH')
-for row in cur: #just testing
-    print(row)
+csvReader = csv.reader(open('player.csv'))
+next(csvReader)
+for row in csvReader:
+    cur.execute('insert into PLAYER(player_id, player_name, dob, batting_hand, bowling_skill, country_name) values (?, ?, ?, ?, ?, ?)', row)
+
+
+csvReader = csv.reader(open('player_match.csv'))
+next(csvReader)
+for row in csvReader:
+    cur.execute('insert into PLAYER_MATCH(playermatch_key, match_id, player_id, batting_hand, bowling_skill, role_desc, team_id) values (?, ?, ?, ?, ?, ?, ?)', row)
+
+
+csvReader = csv.reader(open('ball_by_ball.csv'))
+next(csvReader)
+for row in csvReader:
+    cur.execute('insert into BALL_BY_BALL(match_id, innings_no, over_id, ball_id, striker_batting_position, runs_scored, extra_runs, out_type, striker, non_striker, bowler) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', row)
+
+
+# cur.execute('select player_id, dob from PLAYER')
+# for row in cur: #just testing
+#     print(row)
