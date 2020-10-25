8bc386d663f076f8ff98b0f48ff0f3f8892aded2
Savyasach Reddy <savyasach@cse.iitb.ac.in>
2020-10-25 12:03:41 +0530

    3rd

--- average_runs.py
+++ average_runs.py
@@ -5,8 +5,11 @@ conn=sqlite3.connect('ipl.db')
 
 with conn:
     cur=conn.cursor()
-    cur.execute("""
-
+    cur.execute(""" CREATE TEMP VIEW IF NOT EXISTS looks (
+                        runs,
+                        venue
+                    )
+                    AS
                         SELECT
                             SUM(runs_scored) + SUM(extra_runs), venue_name FROM BALL_BY_BALL INNER JOIN MATCH ON MATCH.match_id = BALL_BY_BALL.match_id
                         GROUP BY
@@ -15,8 +18,22 @@ with conn:
                         ORDER BY
                             SUM(runs_scored) DESC
                     ;""")
+
+    cur.execute("""
+                    SELECT
+                        AVG(runs), venue
+                    FROM
+                        looks
+                    GROUP BY
+                        venue
+                    ORDER BY
+                        AVG(runs) DESC
+                    
+    ;"""
+    )
     one_result = cur.fetchall()
     for i in one_result:
         print("{},{}".format(i[1], i[0]))
+    cur.execute("""DROP VIEW looks;""")
 
     
